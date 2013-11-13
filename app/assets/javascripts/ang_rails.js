dfe5542d2d05ef4e3cb6730ae8be011c4bcfb214
nathanmccartney <nmccartney1231@gmail.com>
2013-11-13 10:33:55 -0500

    406 error fix

--- app/assets/javascripts/ang_rails.js
+++ app/assets/javascripts/ang_rails.js
@@ -1,42 +1,52 @@
+
+
+
 var app;
 
 app = angular.module("angRails", ["ngResource"]);
 
-app.factory("Entry", [
-  "$resource", function($resource) {
-    return $resource("/entries/:id", {
-      id: "@id"
-    }, {
-      update: {
-        method: "PUT"
-      }
-    });
+app.config([
+  '$httpProvider', function($httpProvider) {
+    return $httpProvider.defaults.headers['common']['Accept'] = 'application/json';
   }
 ]);
 
-this.AngRailsCtrl = [
-  "$scope", "Entry", function($scope, Entry) {
-    $scope.entries = Entry.query();
-    $scope.addEntry = function() {
-      var entry;
-      entry = Entry.save($scope.newEntry);
-      $scope.entries.push(entry);
-      return $scope.newEntry = {};
-    };
-    return $scope.drawWinner = function() {
-      var entry, pool;
-      pool = [];
-      angular.forEach($scope.entries, function(entry) {
-        if (!entry.winner) {
-          return pool.push(entry);
-        }
-      });
-      if (pool.length > 0) {
-        entry = pool[Math.floor(Math.random() * pool.length)];
-        entry.winner = true;
-        entry.$update();
-        return $scope.lastWinner = entry;
+
+
+app.factory("Entry", function($resource) {
+  return $resource("/entries/:id", {
+    id: "@id"
+  }, {
+    update: {
+      method: "PUT"
+    }
+  });
+});
+
+
+
+
+this.AngRailsCtrl = function($scope, Entry) {
+  $scope.entries = Entry.query();
+  $scope.addEntry = function() {
+    var entry;
+    entry = Entry.save($scope.newEntry);
+    $scope.entries.push(entry);
+    return $scope.newEntry = {};
+  };
+  return $scope.drawWinner = function() {
+    var entry, pool;
+    pool = [];
+    angular.forEach($scope.entries, function(entry) {
+      if (!entry.winner) {
+        return pool.push(entry);
       }
-    };
-  }
-];
+    });
+    if (pool.length > 0) {
+      entry = pool[Math.floor(Math.random() * pool.length)];
+      entry.winner = true;
+      entry.$update();
+      return $scope.lastWinner = entry;
+    }
+  };
+};
